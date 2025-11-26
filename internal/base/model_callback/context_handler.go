4fe9a7d3841bd26d857e7fcdb73213f4c27029ae
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-11-26 23:40:39 +0800

    non compose init callbacks

--- internal/base/model_callback/context_handler.go
+++ internal/base/model_callback/context_handler.go
@@ -17,17 +17,12 @@ func ContextWrapHandlers(ctx context.Context, handler callbacks.Handler) context
 }
 
 /*
-注入 RunInfo
+1.注入 RunInfo
 RunInfo 也需要注入到 Context 中，才会在触发回调时给到 Handler。
-
-Graph 托管 RunInfo
+a.Graph 托管 RunInfo
 Graph 会为内部所有的 Node 自动注入 RunInfo。机制是每个 Node 的运行，都是一个新的子 Context，Graph 向这个新的 Context 中注入对应 Node 的 RunInfo。
-
-在 Graph 外注入 RunInfo
+b.在 Graph 外注入 RunInfo
 不想使用 Graph，但却想使用 Callback，则：
-
 通过 InitCallbacks(ctx context.Context, info *RunInfo, handlers ...Handler) 获取一个新的 Context 并注入 Handlers 以及 RunInfo。
-
 通过 ReuseHandlers(ctx context.Context, info *RunInfo) 来获取一个新的 Context，复用之前 Context 中的 Handler，并设置新的 RunInfo。
-
 */
