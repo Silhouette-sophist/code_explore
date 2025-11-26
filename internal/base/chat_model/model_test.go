4bb6e313872a6d65d3b00bc9ed91a5c78ab20320
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-11-26 23:15:25 +0800

    rearrange file

--- internal/base/chat_model/model_test.go
+++ internal/base/chat_model/model_test.go
@@ -23,3 +23,19 @@ func TestModel(t *testing.T) {
 	}
 	t.Log(msg)
 }
+
+func TestModelWithCallback(t *testing.T) {
+	ctx := context.Background()
+	model_callback.InitAppendGlobalHandlers(ctx, model_callback.NewChatModelCallback(ctx))
+	model, err := NewChatModel(ctx, DoubaoThinking)
+	if err != nil {
+		t.Fatal(err)
+	}
+	msg, err := model.Generate(ctx, []*schema.Message{
+		schema.UserMessage("eino框架是什么？"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(msg)
+}
---
+++ internal/base/model_callback/aspect.go
@@ -0,0 +1,38 @@
+package model_callback
+
+import (
+	"context"
+
+	"github.com/bytedance/gopkg/util/logger"
+	callbacks2 "github.com/cloudwego/eino/callbacks"
+	"github.com/cloudwego/eino/components/model"
+	"github.com/cloudwego/eino/schema"
+	"github.com/cloudwego/eino/utils/callbacks"
+)
+
+// NewChatModelCallback 使用handlerTemplate包含HandlerHelper，handlerTemplate实现了callbacks.Handler，会基于runInfo对不同模块进行callback
+/**
+type HandlerHelper struct {
+	promptHandler      *PromptCallbackHandler
+	chatModelHandler   *ModelCallbackHandler
+	embeddingHandler   *EmbeddingCallbackHandler
+	indexerHandler     *IndexerCallbackHandler
+	retrieverHandler   *RetrieverCallbackHandler
+	loaderHandler      *LoaderCallbackHandler
+	transformerHandler *TransformerCallbackHandler
+	toolHandler        *ToolCallbackHandler
+	toolsNodeHandler   *ToolsNodeCallbackHandlers
+	composeTemplates   map[components.Component]callbacks.Handler
+}
+*/
+func NewChatModelCallback(ctx context.Context) callbacks2.Handler {
+	return callbacks.NewHandlerHelper().ChatModel(&callbacks.ModelCallbackHandler{
+		OnEnd: func(ctx context.Context, runInfo *callbacks2.RunInfo, output *model.CallbackOutput) context.Context {
+			logger.CtxInfof(ctx, "[OnEndFn] %v %+v", *runInfo, output.TokenUsage)
+			return ctx
+		},
+		OnEndWithStreamOutput: func(ctx context.Context, runInfo *callbacks2.RunInfo, output *schema.StreamReader[*model.CallbackOutput]) context.Context {
+			return ctx
+		},
+	}).Handler()
+}
