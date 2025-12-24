eb62196d6525ef6c1bd75badca09fc07c702c037
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-12-24 23:49:05 +0800

    add embedding and indexer and retriever

--- internal/base/retriever/es8_test.go
+++ internal/base/retriever/es8_test.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"log"
 	"testing"
+
+	"github.com/cloudwego/eino-ext/components/retriever/es8"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
 )
 
 func TestEs8Retriever(t *testing.T) {
@@ -12,9 +15,29 @@ func TestEs8Retriever(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	docs, err := es8Retriever.Retrieve(ctx, "data")
+	docs, err := es8Retriever.Retrieve(ctx, "useful data")
 	if err != nil {
 		log.Panicf("retrieve docs failed, err=%v", err)
 	}
-	log.Println(docs)
+	for i, doc := range docs {
+		t.Logf("doc[%d]: %v", i, doc)
+	}
+
+	caseSense := true
+	docs, err = es8Retriever.Retrieve(ctx, "useful data",
+		es8.WithFilters([]types.Query{{
+			Term: map[string]types.TermQuery{
+				fieldExtraLocation: {
+					CaseInsensitive: &caseSense,
+					Value:           "234",
+				},
+			},
+		}}),
+	)
+	if err != nil {
+		log.Panicf("retrieve docs failed, err=%v", err)
+	}
+	for i, doc := range docs {
+		t.Logf("=doc[%d]: %v", i, doc)
+	}
 }
